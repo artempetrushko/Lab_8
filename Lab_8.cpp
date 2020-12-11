cbbf38d067af5b65909990ccdf8609fed68e67a0
artempetrushko <64064252+artempetrushko@users.noreply.github.com>
2020-12-11 13:08:33 +0500

    Лаб. работа №8

---
+++ Lab_8.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Maze.h"
+
+int main()
+{
+    auto maze = new Maze(5, 5);
+    maze->makeConnection(0, 0, 0, 1);
+    maze->makeConnection(0, 1, 1, 1);
+    maze->makeConnection(1, 1, 1, 2);
+    maze->makeConnection(1, 2, 2, 2);
+    maze->makeConnection(2, 2, 2, 3);
+    maze->makeConnection(2, 3, 3, 3);
+    maze->makeConnection(3, 3, 3, 4);
+    maze->makeConnection(3, 4, 4, 4);
+    maze->printMaze();
+    system("pause");
+    return 0;
+}
---
+++ MCell.cpp
@@ -0,0 +1,17 @@
+#include "MCell.h"
+
+MCell::MCell()
+{
+	m_down = false;
+	m_right = false;
+}
+
+bool MCell::right()
+{
+	return m_right;
+}
+
+bool MCell::down()
+{
+	return m_down;
+}
